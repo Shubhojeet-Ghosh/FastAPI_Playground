373fba7adf82313fa89e3b2d8b993b34c8ec9c5d
Shubhojeet Ghosh <shubhojeet.official@gmail.com>
2025-03-10 15:51:23 +0530

    chore: update environment configuration to include PORT and change HOST to 0.0.0.0

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     PROJECT_VERSION: str = "1.0"
     PORT: int = 8000
     RELOAD: bool = True
-    HOST:str = "127.0.0.1"
+    HOST:str = "0.0.0.0"
     ENVIRONMENT: str
 
     class Config:
